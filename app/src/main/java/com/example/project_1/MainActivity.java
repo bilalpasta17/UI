3e6f38c54d7b178b7d87ae75eda576997d5f7fcd
bilalpasta17 <83414545+bilalpasta17@users.noreply.github.com>
2023-08-09 14:42:34 +0500

    UI

--- app/src/main/java/com/example/project_1/MainActivity.java
+++ app/src/main/java/com/example/project_1/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.project_1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -13,41 +16,82 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView email;
+    private static final int REQUEST_CALL_PERMISSION = 1;
+    private ImageView email, call, chat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        email=findViewById(R.id.img_email);
+        email = findViewById(R.id.img_email);
+        call = findViewById(R.id.call);
+        chat = findViewById(R.id.img_chat);
 
         email.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openEmailClient();
+                email();
+            }
+        });
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                call();
             }
         });
 
+        chat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMessagingApp();
+            }
+        });
     }
 
-    @SuppressLint("QueryPermissionsNeeded")
-    private void openEmailClient() {
-        String emailAddress = "recipient@example.com";
-        String subject = "Sample Subject";
-        String body = "Sample Email Body";
+    private void email() {
+        // Create a new intent to send data
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        // Set the type of data to be sent (wildcard for any type)
+        intent.setType("*/*");
+        // Add the recipient email address
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"test@gmail.com"});
+        // Add the email subject
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Feedback");
+        // Start the activity that can handle the intent (email client)
+        startActivity(intent);
+    }
 
-        // Create the email Uri
-        Uri uri = Uri.parse("mailto:" + emailAddress + "?subject=" + Uri.encode(subject) + "&body=" + Uri.encode(body));
-
-        // Create the email intent
-        Intent emailIntent = new Intent(Intent.ACTION_SENDTO, uri);
-
-        // Verify that there is an email app to handle the intent
-        if (emailIntent.resolveActivity(getPackageManager()) != null) {
-            startActivity(emailIntent);
+    private void call() {
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse("tel:123456789"));
+        // Check if the CALL_PHONE permission is granted
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // Request the permission if not granted
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CALL_PHONE}, REQUEST_CALL_PERMISSION);
         } else {
-            // Handle the case where no email app is available
-            Toast.makeText(this, "No email app found.", Toast.LENGTH_SHORT).show();
+            // Permission already granted, you can proceed with making the call
         }
+        startActivity(callIntent);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CALL_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, you can proceed with making the call
+            } else {
+                // Permission denied, handle it gracefully
+            }
+        }
+    }
+
+    private void openMessagingApp() {
+        // Create a Uri with the scheme "smsto:" to indicate sending an SMS
+        Uri uri = Uri.parse("smsto:");
+        // Create a new intent with the action ACTION_SENDTO and the Uri
+        Intent intent = new Intent(Intent.ACTION_SENDTO, uri);
+        // Start the activity that can handle the intent (default messaging app)
+        startActivity(intent);
     }
 }
